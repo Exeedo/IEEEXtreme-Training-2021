df692349df7f9348c31a69cf247c25b929847243
Osama Khallouf <50551721+Exeedo@users.noreply.github.com>
2021-08-11 22:16:13 +0300

    Adding Python script

---
+++ Lectures/Lecture-2/Sorting.py
@@ -0,0 +1,8 @@
+unsorted_list = [5, 1, 3, 7, -2, 4]
+print("Before sorting:",unsorted_list)
+
+sorted_list = sorted(unsorted_list)
+print("After using 'sorted' function:", unsorted_list, sorted_list)
+
+unsorted_list.sort()
+print("After using 'sort' function:", unsorted_list)
