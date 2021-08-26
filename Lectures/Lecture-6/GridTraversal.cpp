945ab1e9510c2350227a56099802fdaad8ae4066
Osama Khallouf <50551721+Exeedo@users.noreply.github.com>
2021-08-27 00:03:31 +0300

    Added GridTraversal.cpp

---
+++ Lectures/Lecture-6/GridTraversal.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+const int rows = 5;
+const int cols = 6;
+
+int grid[rows][cols];
+bool visited[rows][cols];
+
+int dr[] = { -1,  +1,   0,   0 };
+int dc[] = {  0,   0,  -1,  +1 };
+
+void dfs(int r, int c) {
+	visited[r][c] = true;
+	// for new node in neighbours of (r,c)
+	for (int i = 0; i < 4; i++) {
+		int R = r + dr[i];
+		int C = c + dc[i];
+		if (!visited[R][C])
+			dfs(R, C);
+	}
+
+	return;
+
+}
+
+
+int main() {
+	// inputs => fill for grid
+	dfs(0, 0);
+}
+
+
+/*
+
+ABC
+DXE
+FGH
+
+*/
